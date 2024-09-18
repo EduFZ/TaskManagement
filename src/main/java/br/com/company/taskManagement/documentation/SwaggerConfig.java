68d4f2994bf552e9ceabe90feb54b34dd4dce7c3
Eduardo F Zimmermann <z.edufz@gmail.com>
2024-09-18 00:46:09 -0300

    fix: Correções adicionais da documentation

---
+++ src/main/java/br/com/company/taskManagement/documentation/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package br.com.company.taskManagement.documentation;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TaskManagement")
+                        .version("1.0.0")
+                        .description("Sistema para Gerenciamento de Listas de Tarefas de forma dinâmica."));
+    }
+
+}
